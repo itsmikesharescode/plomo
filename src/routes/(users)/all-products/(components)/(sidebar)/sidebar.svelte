a9b8c3c72e3b25bd8c6a26a8a3085fa224b241d6
Mike John Eviota <eviotamikejohnb@gmail.com>
2025-05-30 19:25:00 +0800

    wips: add wips

---
+++ src/routes/(users)/all-products/(components)/(sidebar)/sidebar.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+</script>
+
+<div class="container mx-auto flex flex-col gap-4 bg-white px-2 py-10">
+	<span class="text-2xl font-semibold">Other Brands</span>
+	<div class="flex flex-wrap gap-2">
+		{#each Array(17)}
+			<a href="/" class="text-sm underline">Sample Brand Name</a>
+		{/each}
+	</div>
+</div>
