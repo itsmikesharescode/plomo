85cb5cd0066a96c20c60559dbddbc6d2354eb787
Mike John Eviota <eviotamikejohnb@gmail.com>
2025-05-31 03:01:13 +0800

    add all products ui
    wips: add wips

--- src/routes/(users)/all-products/(components)/(brands-and-products)/brands-and-products.svelte
+++ src/routes/(users)/all-products/(components)/(brands-and-products)/brands-and-products.svelte
@@ -1,14 +1,11 @@
 <script lang="ts">
+	import Products from './(components)/(products)/products.svelte';
 </script>
 
 <div class="bg-slate-200">
 	<div class="container mx-auto px-2 py-10">
 		{#each Array(22)}
-			<div class="flex flex-col gap-2">
-				<span class="text-xl font-semibold">Rock N Roll</span>
-
-				<div class=""></div>
-			</div>
+			<Products />
 		{/each}
 	</div>
 </div>
